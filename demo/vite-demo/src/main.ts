22faf356bd336052a9bce5d4f2d892c745c0efba
kaanguru <kaanguru@mail.ru>
2024-03-08 23:40:15 +0300

    **Added import statements and updated HTML content in main.ts**

---
+++ demo/vite-demo/src/main.ts
@@ -0,0 +1,20 @@
+import "./style.css";
+import { setupCounter } from "./counter.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
