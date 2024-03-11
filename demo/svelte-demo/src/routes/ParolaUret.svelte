c3e02f899beebcf3ce8097cf287108121bea0281
kaanguru <kaanguru@mail.ru>
2024-03-11 07:47:14 +0300

    Sure! Here is a concise one-line commit message summarizing the changes in the provided code diff:
    "Increased number of words in parolaUret function from 4 to 5 and added button to trigger par

--- demo/svelte-demo/src/routes/ParolaUret.svelte
+++ demo/svelte-demo/src/routes/ParolaUret.svelte
@@ -4,8 +4,9 @@
   let parola = "";
 
   export function generateParola() {
-    parola = parolaUret({ kelimeSayisi: 4, standart: true, araliklar: "-" });
+    parola = parolaUret({ kelimeSayisi: 5, standart: true, araliklar: "-" });
   }
 </script>
 
+<button on:click={generateParola}>Parola Üret</button>
 <h1>{parola}</h1>
