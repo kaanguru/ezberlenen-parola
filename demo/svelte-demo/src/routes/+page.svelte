737201c5fa03732875ee1681a6d96b1dd9101e39
kaanguru <kaanguru@mail.ru>
2024-03-11 07:58:36 +0300

    Remove unnecessary import and button in +page.svelte.

--- demo/svelte-demo/src/routes/+page.svelte
+++ demo/svelte-demo/src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
   import ParolaUret from "./ParolaUret.svelte";
-  import generateParola from "./ParolaUret.svelte";
 </script>
 
 <ParolaUret />
-<button on:click={generateParola}>Parola Üret</button>
